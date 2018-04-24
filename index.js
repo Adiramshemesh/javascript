3cc1831099eabcb0ce42019795ad98746d455daf
Adiram Shemesh <erpshrue>
2018-04-25 01:54:37 +0300

    func getPosts Fixed. still have to fix getPost - something with procces.argv

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-var request = require('request');
+request = require('request');
 
 const URL = 'https://jsonplaceholder.typicode.com/posts/';
 
 module.exports = {
-    getPost, getPosts
+    getPost,
+    getPosts
 };
 
 
 function getPosts() {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         try {
             req(URL).then(async function (data) {
-                    var posts = [];
-                    for (var i = 0; i < data.length; i++) {
+                    let posts = [];
+                    for (let i = 0; i < data.length; i++) {
                         posts[i] = getComments(data[i]);
                     }
-                    return Promise.all(posts).then(getComments(posts)).then(res => {
+                    return Promise.all(posts).then(posts => getComments(posts)).then(res => {
                         console.log(JSON.stringify(res));
                         return resolve(posts.comments);
                     })
@@ -29,28 +30,23 @@ function getPosts() {
     });
 }
 
-
-async function getPost(_id) {
-    return new Promise(async(resolve, reject) => {
+function getPost(_id) {
+    return new Promise(async (resolve, reject) => {
         try {
-            var id;
-            if ( process.argv[2])
-            {   id=process.argv[2];}
-            else {id= _id;}
+            let id = process.argv[1] || _id;
+            let post = await req(URL + id);
+            comments = await req(URL + id + '/comments');
+            post.comments = comments;
+            if (comments.length === 0)
+                return reject("not founded comments");
+            console.log(JSON.stringify(post));
+            return resolve(post);
 
-        //  var id = process.argv[1] || _id;
-          console.log(id);
-        var post = await req(URL + id);
-        var comments = await req(URL + id + '/comments');
-        post.comments = comments;
-        // if(comments.length===0 )//&& post.length===0)
-        //     return reject("not founded post or comments");
-        console.log(JSON.stringify(post));
-    }
-    catch (error) {
-        console.log(error);
-        return reject(error);
-    }
+        }
+        catch (error) {
+            console.log(error);
+            return reject(error);
+        }
     });
 }
 
@@ -58,10 +54,8 @@ async function getPost(_id) {
 async function getComments(data) {
     return new Promise(async (resolve, reject) => {
         try {
-            var comments = await req(URL + data.id + '/comments');
+            comments = await req(URL + data.id + '/comments');
             data.comments = comments;
-          //  if(comments.length===0)
-          //      return reject("not founded comments");
             return resolve(data);
         }
         catch (error) {
@@ -75,8 +69,8 @@ async function req(URL) {
     return new Promise((resolve, reject) => {
         try {
             request(URL, function (error, response, body) {
-                // if(body.length===0)
-                //     return reject("not founded data");
+                if (body.length === 0)
+                    return reject("not founded data");
                 return resolve(JSON.parse(body));
             });
         }
@@ -87,10 +81,3 @@ async function req(URL) {
     });
 }
 
-
-async function main() {
-    //var data = await getPosts();
-    //  var data2= await getPost(9);
-}
-
-main();
